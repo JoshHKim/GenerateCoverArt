3506139b705dff9d5681a617ec561e27ac9b70ec
JoshHKim <71045661+JoshHKim@users.noreply.github.com>
2024-01-23 15:09:32 -0500

    added support

--- GenerateCoverArt.py
+++ GenerateCoverArt.py
@@ -1,20 +1,47 @@
-import lyricsgenius as lg
-file = open("/Users/joshu/OneDrive/Documents/GitHub/GenerateCoverArt/lyrics.txt", "w")
-genius = lg.Genius('1Rc1J7nPas5KWjvKjommVLKOZp287BDDv-lQ-GXYc0XyaD-wPs16D64345WcScLQ', skip_non_songs=True, excluded_terms=["(Remix)", "(Live)"], remove_section_headers=True)
+from openai import OpenAI
+import string 
+import time 
+import re 
+import urllib.request
+from openai import OpenAI
+from dotenv import load_dotenv
 
-artists = ['Logic', 'Rihanna', 'Frank Sinatra']
+load_dotenv()
 
-def get_lyrics(arr, k):  # Write lyrics of k songs by each artist in arr
-    c = 0  # Counter
-    for name in arr:
-        try:
-            songs = (genius.search_artist(name, max_songs=k, sort='popularity')).songs
-            s = [song.lyrics for song in songs]
-            file.write("\n \n   <|endoftext|>   \n \n".join(s))  # Deliminator
-            c += 1
-            print(f"Songs grabbed:{len(s)}")
-        except:  #  Broad catch which will give us the name of artist and song that threw the exception
-            print(f"some exception at {name}: {c}")
+def lyrics(artist,song): 
+    artist = artist.lower() 
+    song = song.lower() 
+    artist = re.sub('[^A-Za-z0-9]+', "", artist) 
+    song = re.sub('[^A-Za-z0-9]+', "", song) 
+    
+    with urllib.request.urlopen("http://azlyrics.com/lyrics/"+str(artist)+"/"+str(song)+".html") as url:
+        s = url.read()
+        string = str(s)
+        split = string.split('<!-- Usage of azlyrics.com content by any third-party lyrics provider is prohibited by our licensing agreement. Sorry about that. -->') 
+        split_html = split[1]
+        split=split_html.split('</div>', 1)
+        lyrics = split[0]
+        lyrics = re.sub('(<.*?>)',"",lyrics) 
+        filter_lyrics=lyrics.replace("\\n", " ")
+        filter_lyrics=filter_lyrics.replace("\\'", "'")
+        filter_lyrics=filter_lyrics.replace("\\r", "")
+        return filter_lyrics
 
 
-get_lyrics(artists, 3)
+
+#artist = input("Enter an artist's name: ")
+#title = input("Enter a song name: ")
+song_lyrics=lyrics("coldplay", "vivalavida")
+
+client = OpenAI()
+
+response = client.images.generate(
+    model="dall-e-3",
+    prompt="create an album cover based on the following lyrics:" + song_lyrics,
+    size="1024x1024",
+    quality="standard",
+    n=1,
+)
+
+image_url = response.data[0].url
+print(image_url)
